90b76de2e7448e76e568c084fac90be5b11bff04
sevta <sevtasaputra@gmail.com>
2020-12-09 18:30:25 +0700

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,104 @@
+import glob from 'glob'
+import path from 'path'
+
+const dynamicContentPath = 'assets/content' // ? No prepending/appending backslashes here
+const dynamicRoutes = getDynamicPaths(
+  {
+    blog: 'blog/*.json',
+    projects: 'projects/*.json'
+  },
+  dynamicContentPath
+)
+
+module.exports = {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
+
+/**
+ * Create an array of URLs from a list of files
+ * @param {*} urlFilepathTable - example below
+ * {
+ *   blog: 'blog/*.json',
+ *   projects: 'projects/*.json'
+ * }
+ *
+ * @return {Array} - Will return those files into urls for SSR generated .html's like
+ * [
+ *   /blog/2019-08-27-incidunt-laborum-e ,
+ *   /projects/story-test-story-1
+ * ]
+ */
+function getDynamicPaths(urlFilepathTable, cwdPath) {
+  console.log('Going to generate dynamicRoutes for these collection types: ', urlFilepathTable)
+  const dynamicPaths = [].concat(
+    ...Object.keys(urlFilepathTable).map(url => {
+      const filepathGlob = urlFilepathTable[url]
+      return glob.sync(filepathGlob, { cwd: cwdPath }).map(filepath => {
+        return `/${url}/${path.basename(filepath, '.json')}`
+      })
+    })
+  )
+  console.log('Found these dynamicPaths that will be SSR generated:', dynamicPaths)
+  return dynamicPaths
+}
