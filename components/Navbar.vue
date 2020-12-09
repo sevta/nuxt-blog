3c221287822f83497fae2e8e8f6e818ab42eb6a0
sevta <sevtasaputra@gmail.com>
2020-12-09 22:15:10 +0700

    edit config yml

---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navbar">
+    <div class="container grid grid-cols-2 mx-auto py-5">
+      <div class="col">
+        <div class="font-bold">LOGO</div>
+      </div>
+      <div class="col flex justify-end items-center">
+        <div
+          class="menu mr-8 text-sm font-sans"
+          v-for="(item, index) in 6"
+          :key="index"
+        >
+          menu
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,39 @@
       <div class="row">
         <h1 class="text-3xl font-sans font-bold">Blog</h1>
       </div>
+      <div class="row flex">
+        <img
+          class="w-24 h-24"
+          v-for="(img, i) in img"
+          :key="i"
+          :src="img.image"
+          alt=""
+        />
+      </div>
       <div class="row flex flex-col mt-8">
         <div class="blog-list mb-3" v-for="(item, key) in doc" :key="key">
           <div class="font-bold">{{ item.title }}</div>
           <div class="text-sm">{{ item.description }}</div>
           <div class="text-sm">link</div>
         </div>
+      </div>
+      <div class="row flex flex-col">
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          class="bg-gray-300 flex flex-col p-5"
+        >
+          <input type="text" name="username" placeholder="username" />
+          <textarea
+            name="body"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="body"
+          ></textarea>
+          <button type="submit">submit</button>
+        </form>
       </div>
     </div>
   </div>
@@ -19,9 +46,11 @@
 export default {
   async asyncData({ $content }) {
     const doc = await $content('blog').fetch()
+    const img = await $content('home-slider').fetch()
 
     return {
-      doc
+      doc,
+      img
     }
   },
   head() {
@@ -33,6 +62,7 @@ export default {
   },
   mounted() {
     console.log('doc', this.doc)
+    console.log('img', this.img)
   }
 }
 </script>
